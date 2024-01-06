9b0e24b4fd22c299049efc4bf5b7263b248135c6
Vodemka <vadimoblogin@mail.ru>
2024-01-06 22:09:10 +0300

    homework 2_3

---
+++ Lesson_2/hw_3/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.WriteLine("print a number: ");
+int x= int.Parse(Console.ReadLine()!);
+if( x<=99 &&x>=10)
+{
+    int a=x/10;
+    int b=x%10;
+    if (a>b)
+    {
+        Console.WriteLine(a);
+    }
+    else
+    {
+        Console.WriteLine(b);
+    }
+}
+Console.WriteLine("Число не лежит на отрезке [10, 99]");
