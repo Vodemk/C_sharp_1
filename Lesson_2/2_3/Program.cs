52752dadf4ad3c01f768f27a94a40062d047d7cf
Vodemka <vadimoblogin@mail.ru>
2024-01-06 21:34:27 +0300

    classwork 2_3

---
+++ Lesson_2/2_3/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("print first number: ");
+int a= int.Parse(Console.ReadLine()!);
+Console.WriteLine("print second number: ");
+int b= int.Parse(Console.ReadLine()!);
+if (a%b==0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет, "+a%b);
+} 
