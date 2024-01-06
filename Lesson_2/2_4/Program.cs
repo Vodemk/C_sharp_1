00e06c64130e0653fe10dc8422f5fec2cd12f08b
Vodemka <vadimoblogin@mail.ru>
2024-01-06 21:40:01 +0300

    classwork 2_4

---
+++ Lesson_2/2_4/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("print first number: ");
+int a= int.Parse(Console.ReadLine()!);
+if(a<100)
+{
+    Console.WriteLine("Третьей цифры нет");
+}
+else
+{
+    Console.WriteLine(a/100%10);
+}
