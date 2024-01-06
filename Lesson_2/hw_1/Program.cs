6a240607766e638ef6fdfb66d8d32d700a2d57ab
Vodemka <vadimoblogin@mail.ru>
2024-01-06 21:45:32 +0300

    homework 2_1

---
+++ Lesson_2/hw_1/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("print first number: ");
+int a= int.Parse(Console.ReadLine()!);
+if (a%7==0 && a%23==0)
+{
+    Console.WriteLine("Да");
+}
+else
+{
+    Console.WriteLine("Нет");
+} 
