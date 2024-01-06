cd3a9b6c1b5678ab14bed1312da61e406ffb006d
Vodemka <vadimoblogin@mail.ru>
2024-01-06 21:55:15 +0300

    homework 2_2

---
+++ Lesson_2/hw_2/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("print first number: ");
+int x= int.Parse(Console.ReadLine()!);
+Console.WriteLine("print second number: ");
+int y= int.Parse(Console.ReadLine()!);
+if (x>0)
+{
+    if (y>0)
+    {
+        Console.WriteLine("1");
+    }
+    else
+    {
+        Console.WriteLine("4");
+    }
+}
+else
+{
+    if (y>0)
+    {
+        Console.WriteLine("2");
+    }
+    else
+    {
+        Console.WriteLine("3");
+    }
+}
