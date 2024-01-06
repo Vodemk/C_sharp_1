d93ba96230debeaaf122f6511c571df1c98ce3ee
Vodemka <vadimoblogin@mail.ru>
2024-01-06 22:21:42 +0300

    homework 2_4

---
+++ Lesson_2/hw_4/Program.cs
@@ -0,0 +1,22 @@
+﻿Console.Write("Введите натуральное число N: ");
+int a = Convert.ToInt32(Console.ReadLine());
+if (a < 10)
+{
+    Console.WriteLine(a);
+}
+else
+{
+    while (a > 0)
+    {
+        int b = a % 10;
+        a /= 10;
+        if (a > 0)
+        {
+            Console.Write(b + ",");
+        }
+        else
+        {
+            Console.WriteLine(b);
+        }
+    }
+}
